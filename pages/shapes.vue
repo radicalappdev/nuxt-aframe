b73eec14755d289ed3d73348a357302041be67b0
Joe Simpson <joe@radicalappdev.com>
2020-08-17 16:56:45 -0400

    Changing the animations on shapes

--- pages/shapes.vue
+++ pages/shapes.vue
@@ -14,39 +14,47 @@
       vr-mode-ui="enterVRButton: #myEnterVRButton;"
     >
       <!-- Style the button with images or whatever. -->
-      <a-assets>
+      <!-- <a-assets>
         <audio id="ambient" src="/audio/Ambient001.mp3" preload="auto"></audio>
-      </a-assets>
+      </a-assets>-->
 
       <a-sound position="0 -1 -1" src="#ambient" autoplay="true" loop="true" on="true"></a-sound>
 
-      <a-dodecahedron
-        position="-20 10 -20"
-        radius="5"
-        material="color: #ff2975; emissive: #ff2975; emissiveIntensity: 0.8; "
-        animation="property: rotation; to: 0 0 -360; loop: true; easing: easeInOutElastic; dur: 5000"
-      ></a-dodecahedron>
+      <!-- <a-entity animation="property: rotation; to: 360 0 0; loop: true; easing: linear; dur: 10000"> -->
+      <a-entity animation="property: rotation; to: 0 360 0; loop: true; easing: linear; dur: 10000">
+        <a-entity rotation="45 0 0" position="-20 10 -20">
+          <a-dodecahedron
+            radius="5"
+            material="color: #ff2975; emissive: #ff2975; emissiveIntensity: 0.8; "
+            animation="property: rotation; to: 0 0 -360; loop: true; easing: linear; dur: 5000"
+          ></a-dodecahedron>
+        </a-entity>
 
-      <a-tetrahedron
-        position="20 10 -20"
-        radius="5"
-        material="color: #ff901f; emissive: #ff901f; emissiveIntensity: 0.8; "
-        animation="property: rotation; to: 360 0 0 ; loop: true; easing: easeInOutSine; dur: 5000"
-      ></a-tetrahedron>
+        <a-entity rotation="45 0 0" position="20 10 -20">
+          <a-tetrahedron
+            radius="5"
+            material="color: #ff901f; emissive: #ff901f; emissiveIntensity: 0.8; "
+            animation="property: rotation; to: 360 0 0 ; loop: true; easing: linear; dur: 5000"
+          ></a-tetrahedron>
+        </a-entity>
 
-      <a-icosahedron
-        position="-20 10 20"
-        radius="5"
-        material="color: #ffd319; emissive: #ffd319; emissiveIntensity: 0.8; "
-        animation="property: rotation; to: 0 360 0 ; loop: true; easing: easeInOutQuad; dur: 5000"
-      ></a-icosahedron>
+        <a-entity rotation="45 0 0" position="-20 10 20">
+          <a-icosahedron
+            radius="5"
+            material="color: #ffd319; emissive: #ffd319; emissiveIntensity: 0.8; "
+            animation="property: rotation; to: 0 360 0 ; loop: true; easing: linear; dur: 5000"
+          ></a-icosahedron>
+        </a-entity>
 
-      <a-octahedron
-        position="20 10 20"
-        radius="5"
-        material="color: #f222ff; emissive: #f222ff; emissiveIntensity: 0.8; "
-        animation="property: rotation; to: 0 0 360 ; loop: true; easing: easeInOutCirc;  dur: 5000"
-      ></a-octahedron>
+        <a-entity rotation="45 0 0" position="20 10 20">
+          <a-octahedron
+            radius="5"
+            material="color: #f222ff; emissive: #f222ff; emissiveIntensity: 0.8; "
+            animation="property: rotation; to: 0 0 360 ; loop: true; easing: linear;  dur: 5000"
+          ></a-octahedron>
+        </a-entity>
+      </a-entity>
+      <!-- </a-entity> -->
 
       <a-entity
         environment="
