69a3844a086a3305ab3f75e5ca0453713e0beae0
Joe Simpson <joe@radicalappdev.com>
2020-08-21 13:05:15 -0400

    Adding more projects

--- store/projects.js
+++ store/projects.js
@@ -2,10 +2,19 @@ export const state = () => ({
     projectData: {
 
 
+
         '9C2DCF89-399B-45D5-9725-326D5009C101': { "date": "2020 - ????", "details": "A WebXR scene where I can remember some of the projects I never finished.", "position": "-2 0 -4", "title": "Project Graveyard" },
         '95324942-E0A1-456C-9D44-7E3C3213CB57': { "date": "2016 - 2017", "details": "My first podcast. My friend Dave and I started recording this in fall of 2016. For years we had been having long phone calls to discuss software development and nerd culture and we decided to start recording those calls. This show eventually morphed into VR Hermits.", "position": "-8 0 -10", "title": "Massively Unqualified: \\nDevelopment" },
         '611AA408-6F31-4EF2-ABB3-7D54FE523312': { "date": "2017 - 2018", "details": "After getting obsessed with VR in the winter of 2017 my friend Dave and I decided to turn refocus our podcast on VR. We kicked this off in June of 2017 and kept it going for about a year before getting distracted with other projects.", "position": "-10 0 -10", "title": "VR Hermits" },
         'E45133B6-B0D1-4B60-BC38-5DD2BA3CBF8E': { "date": "2016 - 2020", "details": "This was an idea for an iOS time tracking app that doesn't require the user to enter start and end times. Instead it just has a series of buttons to add blocks of time to a task for a day.", "position": "-8 0 -15", "title": "Timeless" },
+        '50316DF2-A5F9-40AE-83CD-B3F1FCD0B0BD': { "date": "2020 - 2020", "details": "I wanted to make a simple SwiftUI iOS app where users could react to messages or conversations by using an emoji scale to indicate their feelings", "position": "-10 0 -15", "title": "On a scale of" },
+        '59CC278B-5021-4046-8E8E-4F9124A454D1': { "date": "2018 - 2019", "details": "This was an idea played with when I used to use Apple Reminders for my task managers. I wanted to build an AR app based on EventKit and pull my reminders and lists into AR as cards. I got the basics up and running before uncovering some fundamental flaws in the design. Since that time EventKit has gotten out of sync with the Reminders app and this no longer feels viable at all. Hopefully Apple makes a Reminders app for their AR glasses someday.", "position": "-12 0 -15", "title": "Reminders AR" },
+        '1515F980-EB23-45CE-85A1-19FF95648398': { "date": "2017 - 2018", "details": "A potential collaboration with a local game dev shop. We made some interesting scenes, but it became clear to me that working with them was a dead-end.", "position": "8 0 -10", "title": "Somewhere Else" },
+        'B9B0F4EB-E678-4235-A669-1672B6AC56B0': { "date": "2018 - 2018", "details": "Laser pointers seeking revenge for all of all of the red dots that cats have murdered.", "position": "10 0 -10", "title": "Red Dot Revenge" },
+        'AE360BA0-8022-4A45-ADA2-2B92489E3971': { "date": "2019 - 2019", "details": "A VR endless runner but with falling instead of running.", "position": "12 0 -10", "title": "Turtles all the way down" },
+
+
+
 
     }
 })
