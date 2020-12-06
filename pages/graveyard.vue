16cc43dac598869eb79cfc2f89651680371ff2a1
Joe Simpson <joe@radicalappdev.com>
2020-12-06 07:35:45 -0500

    Removing Enter VR button outside of the canvas

--- pages/graveyard.vue
+++ pages/graveyard.vue
@@ -1,33 +1,56 @@
 <template>
   <div class="sandbox">
     <!-- <a-scene fog="type: linear; color: #9ba8b8; far: 30; near: 1"> -->
-    <div style="height: 5rem;" class="bg-black flex flex-row">
+    <div style="height: 5rem" class="bg-black flex flex-row">
       <div class="text-purple text-2xl flex-1 m-4">Project Graveyard</div>
-      <a
+      <!-- <a
         id="myEnterVRButton"
         class="float-right bg-pink text-white font-semibold rounded px-4 py-2 m-4"
         href="#"
-      >Enter VR</a>
+        >Enter VR</a
+      > -->
     </div>
 
-    <a-scene
+    <a-scene style="height: calc(100vh - 5rem)" embedded>
+      <!-- <a-scene
       style="height: calc( 100vh - 5rem); "
       embedded
       vr-mode-ui="enterVRButton: #myEnterVRButton;"
-    >
+    > -->
       <a-assets>
         <audio id="grey" src="audio/Gray_noise.mp3"></audio>
         <audio id="ambient" src="/audio/Ambient001.mp3" preload="auto"></audio>
 
-        <a-asset-item id="gateway" src="/models/graveyard/Entrance2.obj"></a-asset-item>
-        <a-asset-item id="wall1" src="/models/graveyard/Floor_Modular.obj"></a-asset-item>
-        <a-asset-item id="building" src="/models/graveyard/HouseWithFlatRoof.obj"></a-asset-item>
+        <a-asset-item
+          id="gateway"
+          src="/models/graveyard/Entrance2.obj"
+        ></a-asset-item>
+        <a-asset-item
+          id="wall1"
+          src="/models/graveyard/Floor_Modular.obj"
+        ></a-asset-item>
+        <a-asset-item
+          id="building"
+          src="/models/graveyard/HouseWithFlatRoof.obj"
+        ></a-asset-item>
 
-        <a-asset-item id="column" src="/models/graveyard/Column.obj"></a-asset-item>
-        <a-asset-item id="columnBroken1" src="/models/graveyard/Column_Broken.obj"></a-asset-item>
-        <a-asset-item id="columnBroken2" src="/models/graveyard/Column_Broken2.obj"></a-asset-item>
+        <a-asset-item
+          id="column"
+          src="/models/graveyard/Column.obj"
+        ></a-asset-item>
+        <a-asset-item
+          id="columnBroken1"
+          src="/models/graveyard/Column_Broken.obj"
+        ></a-asset-item>
+        <a-asset-item
+          id="columnBroken2"
+          src="/models/graveyard/Column_Broken2.obj"
+        ></a-asset-item>
 
-        <a-asset-item id="gravestone1" src="/models/graveyard/Gravestone.obj"></a-asset-item>
+        <a-asset-item
+          id="gravestone1"
+          src="/models/graveyard/Gravestone.obj"
+        ></a-asset-item>
       </a-assets>
       <!-- Scene Background -->
 
@@ -76,8 +99,16 @@
 
       <!-- Player -->
       <a-entity id="rig" movement-controls="fly: false; enabled: true;">
-        <a-entity id="camera" camera position="0 1.6 0" look-controls="pointerLockEnabled: true"></a-entity>
-        <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
+        <a-entity
+          id="camera"
+          camera
+          position="0 1.6 0"
+          look-controls="pointerLockEnabled: true"
+        ></a-entity>
+        <a-entity
+          id="leftHand"
+          hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"
+        ></a-entity>
         <a-entity
           id="rightHand"
           hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc"
